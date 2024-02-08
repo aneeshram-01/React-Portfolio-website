353c94c3ed9c5a8f40512a44fe7bee0a07145f73
Aneeshram Bhat <106094134+aneeshram-01@users.noreply.github.com>
2024-02-08 23:56:32 +0530

    Update works.js

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -10,7 +10,7 @@ const Works =()=> {
     return (
         <section id='Works'>
             <h2 className="worksTitle">My Portfolio</h2>
-            <span className="worksDesc">Explain Explain</span>
+            <span className="worksDesc">I take pride in paying attention to the smallest details and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.</span>
             <div className="worksImgs">
                 <img src={Portfolio1} alt='Portfolio1' className="worksImg"/>
                 <img src={Portfolio2} alt='Portfolio2' className="worksImg"/>
