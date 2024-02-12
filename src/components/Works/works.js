b3ec6b373c267f3ceb5104801599fcec09466ada
Aneeshram Bhat <106094134+aneeshram-01@users.noreply.github.com>
2024-02-12 17:57:24 +0530

    Update works.js

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -12,22 +12,31 @@ const Works =()=> {
             <h2 className="worksTitle">My Works</h2>
             <span className="worksDesc">I take pride in paying attention to the smallest details and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.</span>
             <div className="worksImgs">
-         <a href="https://github.com/aneeshram-01/Meridian" target="_blank" rel="noopener noreferrer">
+        
+         <a href="https://github.com/aneeshram-01/Shopper-Categorization" target="_blank" rel="noopener noreferrer">
                 <img src={Portfolio1} alt='Portfolio1' className="worksImg"/>
         </a>
-         <a href="https://github.com/aneeshram-01/Shopper-Categorization" target="_blank" rel="noopener noreferrer">
+        
+         <a href="https://github.com/aneeshram-01/Meridian" target="_blank" rel="noopener noreferrer">
                 <img src={Portfolio2} alt='Portfolio2' className="worksImg"/>
         </a>
-         <a href="https://github.com/aneeshram-01/TriviaHQ" target="_blank" rel="noopener noreferrer">
+        
+         
                 <img src={Portfolio3} alt='Portfolio3' className="worksImg"/>
-        </a>
-         <a href="https://github.com/aneeshram-01/React-Portfolio-website" target="_blank" rel="noopener noreferrer">
+        
+        
+         <a href="https://github.com/aneeshram-01/TriviaHQ" target="_blank" rel="noopener noreferrer">
                 <img src={Portfolio4} alt='Portfolio4' className="worksImg"/>
         </a>
+        
          <a href="https://github.com/aneeshram-01/Video-Piracy-Detection-and-Avoidance" target="_blank" rel="noopener noreferrer">
                 <img src={Portfolio5} alt='Portfolio5' className="worksImg"/>
         </a>
+
+        <a href="https://github.com/aneeshram-01/React-Portfolio-website" target="_blank" rel="noopener noreferrer">
                 <img src={Portfolio6} alt='Portfolio6' className="worksImg"/>
+        </a>
+        
             </div>
             <button className="worksBtn">See More</button>
         </section>
