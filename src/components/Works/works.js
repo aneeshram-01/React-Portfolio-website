f99c77d2c4f0356a137251c8e7bf5dfc3c1e6d71
Aneeshram Bhat <106094134+aneeshram-01@users.noreply.github.com>
2024-02-13 19:36:07 +0530

    Update works.js
    Tile priority

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -12,8 +12,17 @@ const Works =()=> {
             <h2 className="worksTitle">My Work</h2>
             <span className="worksDesc">I take pride in paying attention to the smallest details and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.</span>
             <div className="worksImgs">
-        
-         <a href="https://github.com/aneeshram-01/Shopper-Categorization" target="_blank" rel="noopener noreferrer">
+
+         <a href="https://github.com/aneeshram-01/React-Portfolio-website" target="_blank" rel="noopener noreferrer">
+                <img src={Portfolio6} alt='Portfolio6' className="worksImg"/>
+        </a>
+          <a href="https://nexus-bit.vercel.app/" target="_blank" rel="noopener noreferrer">
+                <img src={Portfolio4} alt='Portfolio4' className="worksImg"/>
+        </a> 
+        <a href="https://github.com/aneeshram-01/Video-Piracy-Detection-and-Avoidance" target="_blank" rel="noopener noreferrer">
+                <img src={Portfolio5} alt='Portfolio5' className="worksImg"/>
+        </a>
+        <a href="https://github.com/aneeshram-01/Shopper-Categorization" target="_blank" rel="noopener noreferrer">
                 <img src={Portfolio1} alt='Portfolio1' className="worksImg"/>
         </a>
         
@@ -24,21 +33,7 @@ const Works =()=> {
          
                 <img src={Portfolio3} alt='Portfolio3' className="worksImg"/>
         
-        
-         <a href="https://nexus-bit.vercel.app/" target="_blank" rel="noopener noreferrer">
-                <img src={Portfolio4} alt='Portfolio4' className="worksImg"/>
-        </a>
-        
-         <a href="https://github.com/aneeshram-01/Video-Piracy-Detection-and-Avoidance" target="_blank" rel="noopener noreferrer">
-                <img src={Portfolio5} alt='Portfolio5' className="worksImg"/>
-        </a>
-
-        <a href="https://github.com/aneeshram-01/React-Portfolio-website" target="_blank" rel="noopener noreferrer">
-                <img src={Portfolio6} alt='Portfolio6' className="worksImg"/>
-        </a>
-        
             </div>
-     
         </section>
     );
 }
