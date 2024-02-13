f3d21f64dd458ac5a37c3c099effcc30fbaf27d4
Aneeshram Bhat <106094134+aneeshram-01@users.noreply.github.com>
2024-02-13 16:10:18 +0530

    Update navbar.js

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -11,16 +11,16 @@ const Navbar=()=>{
             <img src={logo} alt='Logo' className='logo'></img>
             <div className='desktopMenu'>
                 <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
-                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-75} duration={500}className='desktopMenuListItem'>Certificates</Link>
+                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500}className='desktopMenuListItem'>Certificates</Link>
                 <Link activeClass='active' to='Works' spy={true} smooth={true} offset={-100} duration={500}className='desktopMenuListItem'>Projects</Link>
                 <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500}className='desktopMenuListItem'>Contact Me</Link>
             </div>
             <img src={menu} alt='Menu' className='mobMenu' onClick={()=>setShowMenu(!showMenu)}/>
             <div className='navMenu' style={{display: showMenu? 'flex':'none'}}>
                 <Link activeClass='active' to='intro'spy={true} smooth={true} offset={-100} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Home</Link>
-                <Link activeClass='active' to='skills'spy={true} smooth={true} offset={-75} duration={500}className='listItem' onClick={()=>setShowMenu(false)}>Certificates</Link>
+                <Link activeClass='active' to='skills'spy={true} smooth={true} offset={-110} duration={500}className='listItem' onClick={()=>setShowMenu(false)}>Certificates</Link>
                 <Link activeClass='active' to='Works'spy={true} smooth={true} offset={-100} duration={500}className='listItem' onClick={()=>setShowMenu(false)}>Projects</Link>
-                <Link activeClass='active' to='contact'spy={true} smooth={true} offset={-100} duration={500}className='listItem' onClick={()=>setShowMenu(false)}>Contact Me</Link>
+                <Link activeClass='active' to='contact'spy={true} smooth={true} offset={-105} duration={500}className='listItem' onClick={()=>setShowMenu(false)}>Contact Me</Link>
             </div>
         </nav>
     )
