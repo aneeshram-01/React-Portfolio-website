d62c16007fcd5c2e06ac76b6c2c02a3d657ad879
Aneeshram Bhat <106094134+aneeshram-01@users.noreply.github.com>
2024-07-01 12:39:50 +0530

    Update works.js
    Updated tile

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -5,7 +5,7 @@ import Portfolio2 from '../../assets/p8.png';
 import Portfolio3 from '../../assets/p13.png';
 import Portfolio4 from '../../assets/P25.png';
 import Portfolio5 from '../../assets/p7.png';
-import Portfolio6 from '../../assets/p12.png';
+import Portfolio6 from '../../assets/sticky notes.png';
 const Works =()=> {
     return (
         <section id='Works'>
@@ -13,23 +13,23 @@ const Works =()=> {
             <span className="worksDesc">I take pride in paying attention to the smallest details and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.</span>
             <div className="worksImgs">
 
-         <a href="https://github.com/aneeshram-01/React-Portfolio-website" target="_blank" rel="noopener noreferrer">
+         <a href="https://sticky-notes-livid-ten.vercel.app/" target="_blank" rel="noopener noreferrer">
                 <img src={Portfolio6} alt='Portfolio6' className="worksImg"/>
         </a>
         <a href="https://nexus-bit.vercel.app/" target="_blank" rel="noopener noreferrer">
                 <img src={Portfolio4} alt='Portfolio4' className="worksImg"/>
         </a> 
+        <a href="https://github.com/aneeshram-01/Meridian" target="_blank" rel="noopener noreferrer">
+                <img src={Portfolio2} alt='Portfolio2' className="worksImg"/>
+        </a>
         <a href="https://github.com/aneeshram-01/Video-Piracy-Detection-and-Avoidance" target="_blank" rel="noopener noreferrer">
                 <img src={Portfolio5} alt='Portfolio5' className="worksImg"/>
         </a>
         <a href="https://github.com/aneeshram-01/Shopper-Categorization" target="_blank" rel="noopener noreferrer">
                 <img src={Portfolio1} alt='Portfolio1' className="worksImg"/>
         </a>
-         <a href="https://github.com/aneeshram-01/Meridian" target="_blank" rel="noopener noreferrer">
-                <img src={Portfolio2} alt='Portfolio2' className="worksImg"/>
-        </a>
-        
-          <img src={Portfolio3} alt='Portfolio3' className="worksImg"/>
+         
+        <img src={Portfolio3} alt='Portfolio3' className="worksImg"/>
         
             </div>
         </section>
